0262b5bbcb44d7fc52fbba59dffaf67d48441a27
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-03-03 18:09:03 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,20 @@ import { setupRoutes } from "./routes";
 import { env } from "./utils/env";
 
 const app = new Elysia();
+
+// Setup middleware and routes
 setupMiddleware(app);
 setupRoutes(app);
 
-app.listen(env.PORT);
+// Check if running in development or production
+if (process.env.NODE_ENV === "development") {
+    app.listen(env.PORT);
 
-console.log(
-  `API is running at http://${app.server?.hostname}:${app.server?.port}\nAPI Docs : http://${app.server?.hostname}:${app.server?.port}/swagger`
-);
+    console.log(
+        `API is running at http://${app.server?.hostname}:${app.server?.port}\n` +
+        `API Docs : http://${app.server?.hostname}:${app.server?.port}/swagger`
+    );
+} else {
+    // In production (Vercel), export a handler instead of starting the server
+    export const handler = app.handle;
+}
