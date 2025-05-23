30b9b7d6d95dd31ab0c399a8c27eca435c381117
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-05-23 12:06:14 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { setupMiddleware } from "./middlewares";
 import { setupRoutes } from "./routes";
 import { env } from "./utils/env";
 
-const HOST = env.HOST || "0.0.0.0";
 const PORT = Number(env.PORT) || 3000;
+const HOST = env.HOST || "0.0.0.0";
 
 const app = new Elysia();
 setupMiddleware(app);
@@ -12,8 +12,6 @@ setupRoutes(app);
 
 app.listen(PORT, HOST);
 
-
 const displayHost = HOST === "0.0.0.0" ? "localhost" : HOST;
-
 console.log(`API is running at http://${displayHost}:${PORT}`);
 console.log(`API Docs : http://${displayHost}:${PORT}/swagger`);
